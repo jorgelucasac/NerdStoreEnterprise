36dceda17325ee7fcc8d201e5dddf514292f583b
jorgelucas <jorgelucasl91@gmail.com>
2021-05-19 15:39:22 -0300

    [CLEANUP] refatoração e configurações de autenticação no projeto MVC

---
+++ src/web/Estudos.NSE.WebApp.MVC/Configuration/IdentityConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Estudos.NSE.WebApp.MVC.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static void AddIdentityConfiguration(this IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(opt =>
+                {
+                    opt.LoginPath = "/login";
+                    opt.AccessDeniedPath = "/acesso-negado";
+                });
+        }
+
+        public static void UseIdentityConfiguration(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
---
+++ src/web/Estudos.NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Estudos.NSE.WebApp.MVC.Configuration
+{
+    public static class WebAppConfig
+    {
+        public static void AddMvcConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public static void UseMvcConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/erro/500");
+                app.UseStatusCodePagesWithRedirects("/erro/{0}");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseIdentityConfiguration();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Catalogo}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
