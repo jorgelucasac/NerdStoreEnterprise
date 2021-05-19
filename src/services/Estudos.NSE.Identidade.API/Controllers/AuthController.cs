fe177476c87e7e37b630b60d293071a3686bf711
jorgelucas <jorgelucasl91@gmail.com>
2021-05-19 13:48:53 -0300

    [FEATURE] Emitindo JWT pela controller de autenticação
    - emitindo e devolvedo o token ao realizar login e cadastro do usuário

--- src/services/Estudos.NSE.Identidade.API/Controllers/AuthController.cs
+++ src/services/Estudos.NSE.Identidade.API/Controllers/AuthController.cs
@@ -1,7 +1,16 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Estudos.NSE.Identidade.API.Extensions;
 using Estudos.NSE.Identidade.API.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Estudos.NSE.Identidade.API.Controllers
 {
@@ -11,11 +20,15 @@ namespace Estudos.NSE.Identidade.API.Controllers
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly AppSettings _appSettings;
 
-        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        public AuthController(SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager,
+            IOptions<AppSettings> appSettings)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _appSettings = appSettings.Value;
         }
 
         [HttpPost("nova-conta")]
@@ -35,7 +48,7 @@ namespace Estudos.NSE.Identidade.API.Controllers
             if (!result.Succeeded) return BadRequest();
 
             await _signInManager.SignInAsync(user, false);
-            return Ok();
+            return Ok(await GerarJwt(registro.Email));
 
         }
 
@@ -46,10 +59,85 @@ namespace Estudos.NSE.Identidade.API.Controllers
 
             var result = await _signInManager.PasswordSignInAsync(login.Email, login.Senha, false, true);
 
-            if (result.Succeeded) return Ok();
+            if (result.Succeeded) return Ok(await GerarJwt(login.Email));
 
             return BadRequest();
 
         }
+
+        #region privates
+
+
+        private async Task<UsuarioRespostaLogin> GerarJwt(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            var claims = await _userManager.GetClaimsAsync(user);
+
+            var identityClaims = await ObterClaimsUsuario(claims, user);
+            var encodedToken = CodificarToken(identityClaims);
+
+            return ObterRespostaToken(encodedToken, user, claims);
+        }
+
+        private async Task<ClaimsIdentity> ObterClaimsUsuario(ICollection<Claim> claims, IdentityUser usuario)
+        {
+
+            var roles = await _userManager.GetRolesAsync(usuario);
+            claims.Add(new Claim(JwtRegisteredClaimNames.Sub, usuario.Id));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Email, usuario.Email));
+            //id do token
+            claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+            //quando o token foi emitido
+            claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+            //a partir de que horas o token é válido
+            claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow.AddSeconds(3)).ToString()));
+            //até quando o token é válido
+            claims.Add(new Claim(JwtRegisteredClaimNames.Exp, ToUnixEpochDate(DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras)).ToString()));
+
+            roles.ToList().ForEach(r => claims.Add(new Claim("role", r)));
+
+            var identityClaims = new ClaimsIdentity();
+            identityClaims.AddClaims(claims);
+
+            return identityClaims;
+        }
+
+        private string CodificarToken(ClaimsIdentity identityClaims)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _appSettings.Emissor,
+                Audience = _appSettings.ValidoEm,
+                Subject = identityClaims,
+                Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            return tokenHandler.WriteToken(token);
+        }
+
+        private UsuarioRespostaLogin ObterRespostaToken(string encodedToken, IdentityUser user, IEnumerable<Claim> claims)
+        {
+            return new UsuarioRespostaLogin
+            {
+                AccessToken = encodedToken,
+                ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+                UsuarioToken = new UsuarioToken
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c => new UsuarioClaim { Type = c.Type, Value = c.Value })
+                }
+            };
+        }
+
+
+        private static long ToUnixEpochDate(DateTime time)
+        {
+            return (long)Math.Round((decimal)new DateTimeOffset(time.ToUniversalTime()).ToUnixTimeSeconds());
+        }
+        #endregion
     }
 }
