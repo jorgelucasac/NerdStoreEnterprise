4e48df368193bd77ddee7ef08a5bf4c75661c25e
jorgelucas <jorgelucasl91@gmail.com>
2021-05-25 14:11:01 -0300

    [FEATURE] classes base de commands

---
+++ src/building blocks/Estudos.NSE.Core/Messages/Command.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentValidation.Results;
+
+namespace Estudos.NSE.Core.Messages
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        protected virtual bool EhValido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
