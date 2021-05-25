071a426a8d88fa61750fb59a70e0077abe0945ba
jorgelucas <jorgelucasl91@gmail.com>
2021-05-25 12:24:01 -0300

    [FEATURE] Mapeamento das tabelas da API de Clientes

---
+++ src/services/Estudos.NSE.Cliente.API/Data/ClientesContext.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Estudos.NSE.Clientes.API.Models;
+using Estudos.NSE.Core.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Estudos.NSE.Clientes.API.Data
+{
+    public sealed class ClienteDbContext : DbContext, IUnitOfWork
+    {
+        public ClienteDbContext(DbContextOptions<ClienteDbContext> options)
+            : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+            {
+                property.SetColumnType("varchar(100)");
+            }
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.NoAction;
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClienteDbContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            var sucesso = await base.SaveChangesAsync() > 0;
+
+            return sucesso;
+        }
+    }
+}
---
+++ src/services/Estudos.NSE.Cliente.API/Data/Mappings/ClienteMapping.cs
@@ -0,0 +1,42 @@
+﻿using Estudos.NSE.Clientes.API.Models;
+using Estudos.NSE.Core.DomainObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Estudos.NSE.Clientes.API.Data.Mappings
+{
+    public class ClienteMapping : IEntityTypeConfiguration<Cliente>
+    {
+        public void Configure(EntityTypeBuilder<Cliente> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.OwnsOne(c => c.Cpf, tf =>
+            {
+                tf.Property(c => c.Numero)
+                    .IsRequired()
+                    .HasMaxLength(Cpf.CpfMaxLength)
+                    .HasColumnName("Cpf")
+                    .HasColumnType($"varchar({Cpf.CpfMaxLength})");
+            });
+
+            builder.OwnsOne(c => c.Email, tf =>
+            {
+                tf.Property(c => c.Endereco)
+                    .IsRequired()
+                    .HasColumnName("Email")
+                    .HasColumnType($"varchar({Email.EnderecoMaxLength})");
+            });
+
+            // 1 : 1 => Cliiente : Endereco
+            builder.HasOne(c => c.Endereco)
+                .WithOne(c => c.Cliente);
+
+            builder.ToTable("Cliente");
+        }
+    }
+}
