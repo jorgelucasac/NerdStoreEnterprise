4540a6c6be73956ae933fd4282394ab30d0fb9d0
jorgelucas <jorgelucasl91@gmail.com>
2021-05-25 12:35:46 -0300

    [CLEANUP] organizando o arquivo statup de clientes

---
+++ src/services/Estudos.NSE.Cliente.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Estudos.NSE.Clientes.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            
+        }
+    }
+}
