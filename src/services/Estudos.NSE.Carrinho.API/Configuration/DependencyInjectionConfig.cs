f742faa64860eb73f003e0becda360d47d5a0c7d
jorgelucas <jorgelucasl91@gmail.com>
2021-05-27 17:41:54 -0300

    [FEATURE] Reconhecimento do usuário na API de Carrinho

--- src/services/Estudos.NSE.Carrinho.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/Estudos.NSE.Carrinho.API/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,6 @@
 ﻿using Estudos.NSE.Carrinho.API.Data;
+using Estudos.NSE.WebApi.Core.Usuario;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Estudos.NSE.Carrinho.API.Configuration
@@ -8,6 +10,9 @@ namespace Estudos.NSE.Carrinho.API.Configuration
         public static void RegisterServices(this IServiceCollection services)
         {
             services.AddScoped<CarrinhoDbContext>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<IAspNetUser, AspNetUser>();
         }
     }
 }
---
+++ src/services/Estudos.NSE.Carrinho.API/Controllers/CarrinhoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Estudos.NSE.Carrinho.API.Model;
+using Estudos.NSE.WebApi.Core.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Estudos.NSE.Carrinho.API.Controllers
+{
+    [Route("/api/carrinho")]
+    [Authorize]
+    public class CarrinhoController : MainController
+    {
+        [HttpGet]
+        public async Task<CarrinhoCliente> ObterCarrinho()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AdicionarItemCarrinho(CarrinhoItem carrinhoItem)
+        {
+            return CustomResponse();
+        }
+        [HttpPut]
+        public async Task<IActionResult> AtualizarItemCarrinho([FromRoute]Guid produtoId, CarrinhoItem item)
+        {
+            return CustomResponse();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoverItemCarrinho([FromRoute]Guid produtoId)
+        {
+            return CustomResponse();
+        }
+
+
+    }
+}
