58c74325202ab7728026f8c7cc64e1b966f12373
jorgelucas <jorgelucasl91@gmail.com>
2021-06-02 13:32:58 -0300

    [FEATURE] Finalizando a API de Pedido
    - implmenetado o controller de pedido
    - resolvendo as dependências do projeto

--- src/services/Estudos.NSE.Pedido.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/Estudos.NSE.Pedido.API/Configuration/DependencyInjectionConfig.cs
@@ -1,9 +1,14 @@
 ﻿using Estudos.NSE.Core.Mediator;
+using Estudos.NSE.Pedidos.API.Application.Commands;
+using Estudos.NSE.Pedidos.API.Application.Events;
 using Estudos.NSE.Pedidos.API.Application.Queries;
+using Estudos.NSE.Pedidos.Domain.Pedidos;
 using Estudos.NSE.Pedidos.Domain.Vouchers;
 using Estudos.NSE.Pedidos.Infra.Data;
 using Estudos.NSE.Pedidos.Infra.Data.Repository;
 using Estudos.NSE.WebApi.Core.Usuario;
+using FluentValidation.Results;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,15 +18,23 @@ namespace Estudos.NSE.Pedidos.API.Configuration
     {
         public static void RegisterServices(this IServiceCollection services)
         {
-            //API
+            // API
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IAspNetUser, AspNetUser>();
+
+            // Commands
+            services.AddScoped<IRequestHandler<AdicionarPedidoCommand, ValidationResult>, PedidoCommandHandler>();
+
+            // Events
+            services.AddScoped<INotificationHandler<PedidoRealizadoEvent>, PedidoEventHandler>();
 
             // Application
             services.AddScoped<IMediatorHandler, MediatorHandler>();
             services.AddScoped<IVoucherQuerie, VoucherQuerie>();
+            services.AddScoped<IPedidoQueries, PedidoQueries>();
 
             // Data
+            services.AddScoped<IPedidoRepository, PedidoRepository>();
             services.AddScoped<IVoucherRepository, VoucherRepository>();
             services.AddScoped<PedidosDbContext>();
 
---
+++ src/services/Estudos.NSE.Pedido.API/Controllers/PedidoController.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Estudos.NSE.Core.Mediator;
+using Estudos.NSE.Pedidos.API.Application.Commands;
+using Estudos.NSE.Pedidos.API.Application.Queries;
+using Estudos.NSE.WebApi.Core.Controllers;
+using Estudos.NSE.WebApi.Core.Usuario;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Estudos.NSE.Pedidos.API.Controllers
+{
+    [Authorize]
+    [Route("/api/[controller]")]
+    public class PedidoController : MainController
+    {
+        private readonly IMediatorHandler _mediator;
+        private readonly IAspNetUser _user;
+        private readonly IPedidoQueries _pedidoQueries;
+
+        public PedidoController(IMediatorHandler mediator,
+            IAspNetUser user,
+            IPedidoQueries pedidoQueries)
+        {
+            _mediator = mediator;
+            _user = user;
+            _pedidoQueries = pedidoQueries;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AdicionarPedido(AdicionarPedidoCommand pedido)
+        {
+            pedido.ClienteId = _user.ObterUserId();
+            return CustomResponse(await _mediator.EnviarComando(pedido));
+        }
+
+        [HttpGet("ultimo")]
+        public async Task<IActionResult> UltimoPedido()
+        {
+            var pedido = await _pedidoQueries.ObterUltimoPedido(_user.ObterUserId());
+
+            return pedido == null ? NotFound() : CustomResponse(pedido);
+        }
+
+        [HttpGet("lista-cliente")]
+        public async Task<IActionResult> ListaPorCliente()
+        {
+            var pedidos = await _pedidoQueries.ObterListaPorClienteId(_user.ObterUserId());
+
+            return pedidos == null ? NotFound() : CustomResponse(pedidos);
+        }
+    }
+}
