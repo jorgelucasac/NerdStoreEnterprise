cea4a62a34cb12b597ed615845d7e8b7b6325f06
jorgelucas <jorgelucasl91@gmail.com>
2021-06-02 17:48:18 -0300

    [REFACTOR] habilitando deliete cascate nas entidades de carrinho
    - habilitado para excluir o carrinho e os itens juntos

---
+++ src/services/Estudos.NSE.Carrinho.API/Migrations/20210602203545_CarrinhoCascade.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Estudos.NSE.Carrinho.API.Migrations
+{
+    public partial class CarrinhoCascade : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CarrinhoItem_CarrinhoCliente_CarrinhoId",
+                table: "CarrinhoItem");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CarrinhoItem_CarrinhoCliente_CarrinhoId",
+                table: "CarrinhoItem",
+                column: "CarrinhoId",
+                principalTable: "CarrinhoCliente",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CarrinhoItem_CarrinhoCliente_CarrinhoId",
+                table: "CarrinhoItem");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CarrinhoItem_CarrinhoCliente_CarrinhoId",
+                table: "CarrinhoItem",
+                column: "CarrinhoId",
+                principalTable: "CarrinhoCliente",
+                principalColumn: "Id");
+        }
+    }
+}
