aee61b741616f3bd83b7b0557c846224aed75b10
jorgelucas <jorgelucasl91@gmail.com>
2021-06-04 18:35:49 -0300

    [FEATURE] ViewComponent de paginação
    - criado um view component de paginação para ser utilizado em qualquer parte da aplicação

---
+++ src/web/Estudos.NSE.WebApp.MVC/Extensions/PaginacaoViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Estudos.NSE.WebApp.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Estudos.NSE.WebApp.MVC.Extensions
+{
+    public class PaginacaoViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(IPagedList modeloPaginado)
+        {
+            return View(modeloPaginado);
+        }
+    }
+
+   
+}
